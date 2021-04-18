8aeeaf07b3ac0803e57393b54928b7970c823de3
Mathias Duedahl <mathias.duedahl@gmail.com>
2021-04-18 21:26:52 +0200

    Final code assignment 3

--- A3_quadrature.py
+++ A3_quadrature.py
@@ -1,24 +1,45 @@
 import numpy as np
 import scipy.integrate as spi
+import matplotlib.pyplot as plt
 
+#regular simpsons rule
+def simpson(f, a, b):
+    return (b-a)/6*(f(a)+4*f((a+b)/2)+f(b))
 
 def Q(f,a,b):
-    return (b-a)/12*(f(a)+4*f(a+(b-a)/4)+2*f((a+b)/2)+4*f(a+3*(b-a)/4)+f(b))
-
-def asrec(f, a, b, eps, sr):
     m = (a+b)/2
+    return simpson(f,a,m)+simpson(f,m,b)
 
-    if (abs(Q(f,a,b)-sr)<=15*eps):
-        return Q(f,a,b)
-    return asrec(f,a,m,eps,sr) + asrec(f,m,b,eps,sr)
+#Lazy error term
+err = 0
 
+#adaptive simpson recursion, including storage of interval list (div)
+def asrec(f, a, b, delta, div):
+    m = (a+b)/2
+    div.append(m)
+    if (abs(Q(f,a,b)-simpson(f,a,b))<=(b-a)*delta):
+        global err
+        err = err + abs(Q(f,a,b)-simpson(f,a,b))/15
+        return [Q(f,a,b)+(Q(f,a,b)-simpson(f,a,b))/15, div]
+    return [asrec(f,a,m,delta, div)[0] + asrec(f,m,b,delta, div)[0], div]
+
+#function to initialize adaptive simpson.
 def adaptive_simpsons(f,a,b,eps):
-    sr = (b-a)/6*(f(a)+4*f((a+b)/2)+f(b))
-    return asrec(f,a,b,eps,sr)
+    delta = 15*eps/(b-a)
+    div = [a]
+    res, div= asrec(f,a,b,delta, div)
+    div.append(b)
+    div=sorted(div)
+    return res, div
 
+#test function
 def f(x):
     return np.sqrt(abs(x))
 
-print(adaptive_simpsons(f,-1,1,0.005))
-print(spi.quad(f,-1,1))
-
+integral, X = adaptive_simpsons(f,-1,1,0.000005)
+exact = 4/3
+print("Exact error: ",exact-integral,"Estimated error: ", err)
+print(integral, spi.quad(f,-1,1))
+plt.plot(X,[f(x) for x in X], "r")
+plt.plot(X,[0]*len(X),"b.")
+plt.show()
